616210e79ce7d34a4ca05e506213706639d0a426
Pak <pakapongm@gmail.com>
2023-09-30 00:35:16 -0700

    whiteboarding, created an array for document, thinking about using boolean for input alert for button to prompt user

---
+++ Develop/script.js
@@ -0,0 +1,39 @@
+// Assignment code here
+// Maybe create an array with the alphabet inside and special characters?
+// use a method so you only have to use one array
+// maybe start first by making the button work in console.log
+// input 
+// how many variables? 
+// boolean where
+// for loop to keep generating password(interating this array) until desired input (variable for the input)
+// scope... which variables will need a global and which dont
+// math.random and math.floor to create a RNG to select randomly in the array
+// work up from console.log problems 
+// instead of using two arrays one for upper and lower use RNG to pick toupper and tolower
+// 
+
+if (specChar === true){
+  console.log("they want special char true")
+} else {
+  console.log("they dont want spec char false")
+}
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+const document = []
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  password = ''
+  passwordText.value = password;
+  document = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z",'!',`"`,"#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[",\,"]","^","_","`","{","|","}","~"]
+  // i cant use an array for special characters?? 
+  // string instead idk i wanna try array again
+
+}
+
+console.log(writePassword)
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Passpassgen
 Password generator using javascript
+
+Block special characters
+https://stackoverflow.com/questions/24774367/how-to-validate-html-textbox-not-to-allow-special-characters-and-space
