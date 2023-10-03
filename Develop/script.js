d8edf7c8274518bba5080e107096fb4a025c6b70
Pak <pakapongm@gmail.com>
2023-10-02 23:32:56 -0700

    created prompt booleans, continue the rest and then create another function for password length

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,29 @@
 // math.random and math.floor to create a RNG to select randomly in the array
 // work up from console.log problems 
 // instead of using two arrays one for upper and lower use RNG to pick toupper and tolower
-//
+//GIVEN I need a new, secure password
+
+/*WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+*/
+
+// step 1: set up user input
+// step 2: then we add user selection
+// step 3: customizable length and characters
+// step 4: validate user input
+// step 5: generate and test password to display
 
 /*
 if (specChar === true){
@@ -20,39 +42,43 @@ if (specChar === true){
   console.log("they dont want spec char false")
 }
 */
-char = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
-specChar = ['!',`"`,"#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]
-nums = ["0","1","2","3","4","5","6","7","8","9"]
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-var passwordText = {
-  char: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"],
-  specChar: ['!',`"`,"#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"],
-  nums: ["0","1","2","3","4","5","6","7","8","9"]
+var length = prompt("Password Length 8-128")
+var specChar = prompt("Special Characters? yes/no")
+var lowerCase = prompt("Include Lowercase? yes/no")
+var upperCase = prompt("Include Uppercase? yes/no")
+var numeric = prompt("Include Numbers? yes/no")
+var yes = true;
+var no = false;
+function userInput(){
+  if(specChar === true){
+    specChar(Math.random(Math.floor))
+  }
 }
 
-
+// to do next time, write out booleans and learn math random syntax
 // Write password to the #password input  
-function writePassword(generatePassword) {
-  var password = generatePassword(passwordText);
+// make a function for password length
+function writePassword() {
+  var password = generatePassword();
   passwordText.value = password;
 
   var passwordText = document.querySelector("#password");
 
-  var passwordText = {
-    char: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"],
-    specChar: ['!',`"`,"#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"],
-    nums: ["0","1","2","3","4","5","6","7","8","9"]
-  }
+    char = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"],
+    specChar = ['!',`"`,"#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"],
+    nums = ["0","1","2","3","4","5","6","7","8","9"]
+
   
   for (i=0; passwordText.length;i++) {
-    var generatePassword = passwordText [Math.floor(Math.random)]
+    var generatePassword[] = passwordText [Math.floor(Math.random)]
   }
   return ; 
 }
 //generatePassword is a variable that holds the password after its generated 
 console.log()
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword,char, specChar, nums)
+generateBtn.addEventListener("click", writePassword,char, specChar, nums) 
 
